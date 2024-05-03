a0a69ade0e344d7f0f7aee29e2871b5104cd55a9
opilane <opilane@HP-CZC0359NND>
2024-05-03 11:59:07 +0300

    Add project files.

---
+++ forloop/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace forloop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Mingi tekst");
+            }
+        }
+    }
+}
